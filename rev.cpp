743d68201286d0a87b72079b0618f55dabf99912
yashwingahlawat <yashwingahlawat@gmail.com>
2025-08-10 16:13:54 +0530

    ok

---
+++ rev.cpp
@@ -0,0 +1,125 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+   /* int a;
+    cin >> a;
+
+    //cout << "value of n is" << " "<< n << endl;
+    if (a>0) {
+        cout << "a is positive" << endl;
+    }
+
+        else {
+            cout <<"a is negative" << endl;
+        }
+            */
+    /*int a,b;
+    cout << "enter the value of a" << endl;
+    cin>>a;
+
+    cout << "enter the value of b" << endl;
+    cin >> b;
+
+    if(a>b) {
+        cout << "a is greater" << endl;
+        
+    }
+    if (a<b) {
+        cout << "b is greater" << endl;
+    }
+    if(a==b) {
+        cout << " none of em is greater as they are equal" << endl;
+    }
+        
+    
+
+    int a=2;
+    
+    
+
+    int b = a+1;
+    
+
+    if((a==3)==b) {
+        cout << a;
+    }
+    else {
+        cout << a+1;
+    }
+    return 0;
+    
+   int a = 26;
+   
+   if(a<24) {
+    cout << "OKAY" << endl;
+   }
+   else if(a == 24) {
+    cout << "NOT OKAY" << endl;
+   }
+   else {
+    cout << "NA" << endl;
+   }
+    
+   char ch;
+   cout << "enter the character";
+   cin>>ch;
+
+   if(ch>='A' && ch<='Z' ) {
+    cout << "UPPER CASE" << " ";
+   }
+   else if(ch>='a' && ch<='z') {
+    cout << "LOWER CASE"<< " ";
+   }
+   else if(ch>='0' && ch<='9') {
+    cout << "NUMERICAL VALUE" << " ";
+   }
+   else{
+    cout << "TRY AGAIN";
+   }
+    
+   int n;
+   cin >> n;
+
+   int i = 1;
+   int sum = 0;
+
+   while(i<=n) {
+    sum = sum + i;
+    i = i + 1;
+
+
+   }
+   cout << "Value of sum is" << " ";
+
+    int n;
+    cout << " enter the value of n : " << endl;
+    cin >> n;
+
+    int i = 2;
+    int sum = 0;
+
+    while(i<=n) {
+        cout << i << " ";
+
+    
+    if((i%2)==0) {
+        sum = sum + i;
+    }
+    i = i + 2;
+    }
+    cout << "\n Sum of even no.s from 1 to "<< n <<"is:" << sum << endl;
+    */
+   float c,f;
+   cout << "enter the value of temp in f : " << endl;
+   cin>>f;
+
+   c = 5*(f-32)/9;
+
+   cout << "the value in c is: "<<c<<"degree"<<endl;
+
+}
+
+    
+
+
